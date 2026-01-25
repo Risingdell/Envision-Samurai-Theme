c7a6701a275e97055c9bed44e00dc077410c4d48
Manish-G-suvarna <manishgsuvarna44@gmail.com>
2026-01-25 14:47:23 +0530

    Restore backend folder from main

---
+++ envision-new/backend/routes/teamRoutes.js
@@ -0,0 +1,27 @@
+import express from "express";
+import db  from "../db.js";
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  const query = `
+    SELECT 
+      core_team.id,
+      core_team.name,
+      core_team.role,
+      core_team.image_url,
+      teams.team_name
+    FROM core_team
+    JOIN teams ON core_team.team_id = teams.id
+    ORDER BY teams.team_name, core_team.display_order
+  `;
+
+  db.query(query, (err, result) => {
+    if (err) {
+      return res.status(500).json(err);
+    }
+    res.json(result);
+  });
+});
+
+export default router;
---
+++ envision-new/backend/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import  db  from "./db.js";
+import teamRoutes from "./routes/teamRoutes.js";
+import eventRoutes from "./routes/eventRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/team", teamRoutes);//team routes
+app.use("/api/events", eventRoutes);//events routes
+app.use("/api/orders", orderRoutes);//order routes 
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
