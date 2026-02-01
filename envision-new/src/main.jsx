7d07d94b654495768a3680c2f5b1f04886da3848
Manish-G-suvarna <manishgsuvarna44@gmail.com>
2026-02-01 09:18:48 +0530

    Fix import statements to use .jsx extensions

--- envision-new/src/main.jsx
+++ envision-new/src/main.jsx
@@ -2,12 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
-import App from './App.tsx'
-import Events from './pages/Events.tsx'
-import Profile from './pages/Profile.tsx'
-import Layout from './components/Layout.tsx'
+import App from './App.jsx'
+import Events from './pages/Events.jsx'
+import Profile from './pages/Profile.jsx'
+import Layout from './components/Layout.jsx'
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
